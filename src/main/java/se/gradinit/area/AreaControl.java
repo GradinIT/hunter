9bbea802f0493b663b0d20dde767a08ff353af95
Joakim Gradin <joakimgradin@Apoc.local>
2025-04-02 22:19:44 +0200

    framework

--- src/main/java/se/gradinit/area/AreaControl.java
+++ src/main/java/se/gradinit/area/AreaControl.java
@@ -1,4 +1,12 @@
 package se.gradinit.area;
 
+import org.springframework.web.bind.annotation.RestController;
+import se.gradinit.area.service.AreaService;
+@RestController
 public class AreaControl {
+    private final AreaService areaService;
+
+    public AreaControl(AreaService areaService) {
+        this.areaService = areaService;
+    }
 }
--- src/main/java/se/gradinit/area/mapper/AreaMapper.java
+++ src/main/java/se/gradinit/area/mapper/AreaMapper.java
@@ -3,11 +3,16 @@ package se.gradinit.area.mapper;
 import se.gradinit.area.dao.AreaEntity;
 import se.gradinit.area.model.Area;
 
+import java.util.List;
+
 public class AreaMapper {
-    public static Area map(AreaEntity entity){
+    public static Area map(AreaEntity area){
         return Area.builder().build();
     }
     public static AreaEntity map(Area area){
         return AreaEntity.builder().build();
+    }
+    public static List<Area> map(List<AreaEntity> areas){
+        return areas.stream().map(AreaMapper::map).toList();
     }
 }
