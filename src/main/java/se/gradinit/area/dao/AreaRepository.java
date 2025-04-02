30ef498c3e8674295eed043ccc0075365f4aec00
Joakim Gradin <joakimgradin@Apoc.local>
2025-04-02 23:19:34 +0200

    framework

--- src/main/java/se/gradinit/area/dao/AreaRepository.java
+++ src/main/java/se/gradinit/area/dao/AreaRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AreaRepository extends JpaRepository<Long,AreaEntity> {
+public interface AreaRepository extends JpaRepository<AreaEntity,Long> {
 }
