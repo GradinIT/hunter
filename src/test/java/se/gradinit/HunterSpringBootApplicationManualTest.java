01afc9cc9086ee96ddc5a2e3ec12ca4543f89e3a
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-04-07 10:53:36 +0200

    disable cors for test

--- src/test/java/se/gradinit/HunterSpringBootApplicationManualTest.java
+++ src/test/java/se/gradinit/HunterSpringBootApplicationManualTest.java
@@ -2,11 +2,22 @@ package se.gradinit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HunterSpringBootApplicationManualTest {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(HunterSpringBootApplicationManualTest.class);
         app.run(args);
+    }
+
+    @Configuration
+    public static class HunterTestConfiguration implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedMethods("*");
+        }
     }
 }
