1b7607d303cdbab9681c5a4afeba99d93079f268
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-04-23 09:41:45 +0200

    Find observation by date

--- src/main/java/se/gradinit/observation/dao/ObservationRepository.java
+++ src/main/java/se/gradinit/observation/dao/ObservationRepository.java
@@ -3,6 +3,10 @@ package se.gradinit.observation.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface ObservationRepository extends JpaRepository<ObservationEntity, Long> {
+    List<ObservationEntity> findByDate(LocalDate date);
 }
