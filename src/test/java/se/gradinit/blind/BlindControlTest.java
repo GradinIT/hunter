b370b1ef44ef54294ee540a50b17fc510042ed37
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-04-29 12:47:14 +0200

    Mock login user

--- src/test/java/se/gradinit/blind/BlindControlTest.java
+++ src/test/java/se/gradinit/blind/BlindControlTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(classes= HunterSpringBootApplication.class)
+@WithMockUser(roles = "ADMIN")
 public class BlindControlTest {
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/se/gradinit/observation/ObservationControlTest.java
+++ src/test/java/se/gradinit/observation/ObservationControlTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(classes= HunterSpringBootApplication.class)
+@WithMockUser(roles = "ADMIN")
 public class ObservationControlTest {
     @Autowired
     private MockMvc mockMvc;
