55a53ac71a11364cf022a57ad3358c1caf1b8981
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-04-29 12:03:53 +0200

    Login admin (#16)

--- src/main/java/se/gradinit/HunterApplicationConfiguration.java
+++ src/main/java/se/gradinit/HunterApplicationConfiguration.java
@@ -1,9 +1,62 @@
 package se.gradinit;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableScheduling
+@EnableWebSecurity
 public class HunterApplicationConfiguration {
+    @Value("${app.security.admin.password:}")
+    private String adminPassword;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(CsrfConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/styles.css").permitAll()
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .formLogin(form -> form
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/index.html")
+                        .loginProcessingUrl("/login")
+                        .loginPage("/login.html")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService configureAdmin(PasswordEncoder passwordEncoder) {
+        if (adminPassword.isEmpty()) {
+            throw new IllegalArgumentException("Admin password must be set in application properties");
+        }
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode(adminPassword))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
