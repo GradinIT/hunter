af0df509301dc9bf4b2e3a34425a715ca74dafc6
Olle Mattsson <rymdolle@users.noreply.github.com>
2025-05-02 01:50:54 +0200

    Login tests (#17)
    * Mock login user
    
    * Round add button corners

--- src/test/java/se/gradinit/area/AreaControlTest.java
+++ src/test/java/se/gradinit/area/AreaControlTest.java
@@ -5,18 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import se.gradinit.HunterSpringBootApplication;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(classes= HunterSpringBootApplication.class)
+@WithMockUser(roles = "ADMIN")
 public class AreaControlTest {
 
     @Autowired
